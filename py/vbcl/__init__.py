fd7f31149f05ae107c5a7c39b9f992366a82ba06
Urs Liska <git@ursliska.de>
2017-05-24 18:16:47 +0200

    vbcl-py: Factor out parse() from parse_file()
    This will make the approach more flexible

--- py/vbcl/__init__.py
+++ py/vbcl/__init__.py
@@ -15,45 +15,60 @@ list_items_start = re.compile(r"^(.*): \[")
 list_items_end = re.compile(r"^  \]")
 
 
-def parse_file(file):
-    """Returns a dictionary corresponding to a parsed VBCL config file."""
-
-    d = dict()
+def parse(lines):
+    """Returns a dictionary corresponding to a parsed VBCL string list."""
+    d = {}
+    i = -1
+    k = -1
     
-    with open(file) as f:
-        for line in f:
-            
-            # comments - discard
-            if comment.search(line):
+    for line in lines:
+        i += 1
+        if k > i:
+            continue
+
+        # comments - discard
+        if comment.search(line):
+            continue
+        else:
+            # long text
+            m = long_text_start.search(line)
+            if m:
+                j = -1
+                text = str()
+                for line in lines[i+1:]:
+                    j += 1
+                    if long_text_end.search(line):
+                        d[m.group(1)] = text.strip('\n')
+                        k = i + j
+                        break
+                    text += line.strip(' ') + ' ' # add only a single space
                 continue
             else:
-                # long text
-                m = long_text_start.search(line)
+                # list
+                m = list_items_start.search(line)
                 if m:
-                    text = str()
-                    for line in f:
-                        if long_text_end.search(line):
-                            d[m.group(1)] = text.strip('\n')
+                    j = -1
+                    items = list()
+                    for line in lines[i+1:]:
+                        j += 1
+                        if list_items_end.search(line):
+                            d[m.group(1)] = items
+                            k = i + j
                             break
-                        text += line.strip(' ')
+                        items.append(line.strip(' \n'))
                     continue
                 else:
-                    # list
-                    m = list_items_start.search(line)
+                    # name value pair
+                    m = nv_pair.search(line)
                     if m:
-                        items = list()
-                        for line in f:
-                            if list_items_end.search(line):
-                                d[m.group(1)] = items
-                                break
-                            items.append(line.strip(' \n'))
-                        continue
-                    else:
-                        # name value pair
-                        m = nv_pair.search(line)
-                        if m:
-                            d[m.group(1).strip()] = m.group(2).strip()
+                        d[m.group(1).strip()] = m.group(2).strip()
+    return d
+    
+def parse_file(filename):
+    """Returns a dictionary corresponding to a parsed VBCL config file."""
+
+    with open(filename) as f:
+        return parse(f.read().split('\n'))
+    f.close()
             
-        f.close()
-        return d
      
