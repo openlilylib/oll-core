a5f1fd1d1ba80d77834ad881a60177791358da18
Andrew Bernard <andrew.bernard@gmail.com>
2017-05-26 16:53:15 +1000

    Module for Scheme iterator for lists

---
+++ scheme/iter.scm
@@ -0,0 +1,24 @@
+;; Iterators
+;;
+;; Andrew Bernard 2017
+
+(define-module (oll-core scheme iterator))
+
+;; list iterator
+(define list-iter
+  (lambda (lis)
+    (define iter
+      (lambda ()
+        (call-with-current-continuation control-state)))
+    (define control-state
+      (lambda (return)
+        (for-each
+	 (lambda (element)
+	   (set! return (call-with-current-continuation
+			 (lambda (resume-here)
+			   (set! control-state resume-here)
+			   (return element)))))
+	 lis)
+        (return 'list-ended)))
+    iter))
+
