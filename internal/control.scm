2d3fa0d7efd65032b45f224e4e739472521bfd05
Jean Abou Samra <jean@abou-samra.fr>
2022-08-14 10:13:58 +0200

    control.scm: only define when and unless if running Guile 1
    Guile 2 has them by default, and this definition fails because the
    (ice-9 syncase) module has been removed (since its functionality is
    available without loading anything).

--- internal/control.scm
+++ internal/control.scm
@@ -5,20 +5,18 @@
 
 (define-module (oll-core internal control))
 
-(use-syntax (ice-9 syncase))
-
 ;; when and unless from R6RS
-
-(define-syntax when
-  (syntax-rules ()
-    ((when test result1 result2 ...)
-     (if test
-	 (begin result1 result2 ...)))))
-
-
-(define-syntax unless
-  (syntax-rules ()
-    ((unless test result1 result2 ...)
-     (if (not test)
-	 (begin result1 result2 ...)))))
-
+(cond-expand
+ (guile-2) ;; when and unless are already available on Guile 2
+ (else
+  (use-syntax (ice-9 syncase))
+  (define-syntax when
+    (syntax-rules ()
+      ((when test result1 result2 ...)
+       (if test
+           (begin result1 result2 ...)))))
+  (define-syntax unless
+    (syntax-rules ()
+      ((unless test result1 result2 ...)
+       (if (not test)
+           (begin result1 result2 ...)))))))
