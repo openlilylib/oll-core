6362634d4115ee4d1ff4de79fc54278509e70b16
Jean Abou Samra <jean@abou-samra.fr>
2022-09-21 12:30:37 +0200

    Avoid call/cc
    call/cc is partially broken on recent Windows binaries of LilyPond,
    see https://gitlab.com/lilypond/lilypond/-/issues/6430.  Work around
    it for now.

--- internal/iterator.scm
+++ internal/iterator.scm
@@ -1,25 +1,15 @@
 ;; Iterators
-;;
-;; Andrew Bernard 2017
 
 (define-module (oll-core internal iterator))
-(export list-iter)
+(use-modules (ice-9 match))
 
 ;; list iterator
-(define list-iter
-  (lambda (lis)
-    (define iter
-      (lambda ()
-        (call/cc control-state)))
-    (define control-state
-      (lambda (return)
-        (for-each
-	 (lambda (element)
-	   (set! return (call/cc
-			 (lambda (resume-here)
-			   (set! control-state resume-here)
-			   (return element)))))
-	 lis)
-        (return 'list-ended)))
-    iter))
-
+(define-public (list-iter lis)
+  (let ((state lis))
+    (lambda ()
+      (match state
+        ((a . b)
+         (set! state b)
+         a)
+        (()
+         'list-ended)))))
